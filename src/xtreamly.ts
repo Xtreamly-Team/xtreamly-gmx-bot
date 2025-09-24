705524812f438cc4c9561ee0adbf6d9490b4780c
Eren Nevin <eren.m.nevin@gmail.com>
2025-09-24 13:00:05 +0300

    Fixed signal deserialization

--- src/xtreamly.ts
+++ src/xtreamly.ts
@@ -1,5 +1,14 @@
 require('dotenv').config();
 
+interface Signal {
+    symbol: string
+    long: boolean
+    short: boolean
+    horizon: number
+    stop_loss: number
+    take_profit: number
+}
+
 export class Xtreamly {
     private baseUrl: string;
     constructor() {
@@ -21,19 +30,24 @@ export class Xtreamly {
         }
         const resObj = await res.json()
         console.log(`Raw signal res obj: ${resObj}`)
+        console.log(`Raw single signal: ${resObj[0]}`)
+        const signals: Signal[] = resObj.map((s) => {
+            return {
+                symbol: s.symbol,
+                long: s.signal_long,
+                short: s.signal_short,
+                horizon: s.horizon,
+                stop_loss: s.stop_loss,
+                take_profit: s.take_profit,
+            }
+        })
         // const signals: Signal[] = JSON.parse(resObj).map((signal: any) => ({
         //     symbol: signal.symbol,
         //     long: signal.signal_long,
         //     short: signal.signal_short,
         //     horizon: signal.horizon,
         // }));
-        return resObj
+        return signals
     }
 }
 
-interface Signal {
-    symbol: string
-    long: boolean
-    short: boolean
-    horizon: number
-}
