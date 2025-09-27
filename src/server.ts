0b8ec8fdb2120b7adbaa118faa6faa26a08be44e
Mohammad Hossein Malek <MHOS.MALEK@GMAIL.COM>
2025-09-27 19:58:48 +0200

    feat: integrate Sentry for error monitoring and update deployment

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,35 @@
 import express from "express";
+import * as Sentry from "@sentry/node";
+import { ProfilingIntegration } from "@sentry/profiling-node";
 import swaggerUi from "swagger-ui-express";
 import swaggerJSDoc from "swagger-jsdoc";
+import { SENTRY_DSN } from "./config";
 
 import { runPerpetualStrategy } from "./run_perpetual.js";
 
 const app = express();
+
+Sentry.init({
+  dsn: SENTRY_DSN,
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Sentry.Integrations.Express({ app }),
+    new ProfilingIntegration(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0,
+  // Set sampling rate for profiling - this is relative to tracesSampleRate
+  profilesSampleRate: 1.0,
+});
+
+// The request handler must be the first middleware on the app
+app.use(Sentry.Handlers.requestHandler());
+
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler());
+
 app.use(express.json());
 const port = parseInt(process.env.PORT || "3000", 10);
 
@@ -92,6 +117,16 @@ const swaggerSpec = swaggerJSDoc({
 
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
+// The error handler must be registered before any other error middleware and after all controllers
+app.use(Sentry.Handlers.errorHandler());
+
+// Optional fallthrough error handler
+app.use(function onError(err, req, res, next) {
+  // The error id is attached to `res.sentry` to be returned
+  // and optionally displayed to the user for support.
+  res.statusCode = 500;
+  res.end(res.sentry + "\n");
+});
 
 app.listen(port, "0.0.0.0", () => {
   console.log(`Server running at port ${port}`);
